c4972bda0b8a483e7fce7aed120230457c6a3fd7
Raúl Rodríguez Trotonda <raul.rodriguez.trotonda@gmail.com>
2025-07-04 17:31:58 +0200

    ControlPanel class creation

---
+++ Zork/ControlPanel.cpp
@@ -0,0 +1,5 @@
+#include "ControlPanel.h"
+
+ControlPanel::ControlPanel(string name, string description, bool isPickeable, bool isStoreable) : Item(name, description, isPickeable, isStoreable)
+{
+}
---
+++ Zork/ControlPanel.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Item.h"
+class ControlPanel : public Item
+{
+private:
+
+public:
+	ControlPanel(string name, string description, bool isPickeable, bool isStoreable);	//Constructor of the class Bag with no items inside
+};
+
